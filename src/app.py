b4ce8f417ec861083839a5e27834dfb197d5bd98
Nikul Nayi <nayinikul97@gmail.com>
2023-03-24 21:08:12 -0400

    Entry point Prediction Pipline script

---
+++ src/app.py
@@ -0,0 +1,22 @@
+import pandas as pd
+#import predict
+import numpy as np
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
+from pathlib import Path
+from sklearn.pipeline import Pipeline
+import dill
+from predict import predict
+project_dir = Path(__file__).resolve().parents[1]
+with open(project_dir.joinpath('notebooks/pipelines/PreprocessingPipeline2.0.pkl'), 'rb') as f:
+    pipeline, categories = dill.load(f)
+# Define the Streamlit app
+def main():
+    # Create a file uploader for CSV files
+    
+    data = pd.read_csv(project_dir.joinpath('data/raw/test.csv'),  encoding= 'unicode_escape')
+    data = data.drop(["Unusual"], axis=1)
+    prediction = predict(data)
+    print(prediction)
+# Run the app
+if __name__ == '__main__':
+    main()
