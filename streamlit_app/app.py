ba44f353be637a89a8f028d0e8c60d8e408d71eb
Reshma M <reshmamuralidharanpillai@gmail.com>
2023-03-28 23:37:57 -0400

    Slight changes to Management View

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import time
 import matplotlib.pyplot as plt
+import plotly.express as px
 import altair as alt
 import numpy as np
 from pathlib import Path
@@ -14,7 +15,7 @@ import dill
 from sklearn import set_config
 
 # from model
-from xgboost import XGBClassifier
+from xgboost import XGBClassifier,plot_importance
 
 import sys
 
@@ -50,9 +51,13 @@ with st.sidebar:
     opt = st.radio(label = 'Who is viewing', options = ['Management','Data Science Team','Summary'])
     
 
-
+ 
 if file is not None:
     df = pd.read_csv(file,encoding='windows-1254')
+
+    x = df.iloc[:, :-1]  # Using all column except for the last column as X
+    y = df.iloc[:, -1]  # Selecting the last column as Y
+
     if opt == 'Management':
         with st.spinner('Processing..'):
     # Do some time-consuming computation
@@ -70,17 +75,17 @@ if file is not None:
         # create the columns 
         kpi1,kpi2 = st.columns(2)
         kpi1.metric(
-            label="Count of Nomal Cell Tower",
+            label="Count of Usual Cell Tower Activity",
             value=count_normal
         )
         kpi2.metric(
-            label="Count of Abnormal Cell Tower",
+            label="Count of Unusual Cell Tower Activity",
             value=count_abnormal
         )
         col1,col2=st.columns(2)
         with col1:
             # create a pie chart with the data
-            st.text("Percentage share of Usual and Unusual")
+            st.text("Percentage share of Usual and Unusual Activity")
             sizes = [count_normal,count_abnormal]
             explode = (0, 0.1)
             fig1, ax1 = plt.subplots()
@@ -93,15 +98,41 @@ if file is not None:
         with col2:
             # Bar graph
             # group by 'Cell Name' and count the number of occurrences of 0 and 1
-            counts = df.groupby(['CellName', 'Unusual'])['Unusual'].count()
-
+            
+            counts = (df.groupby(['CellName', 'Unusual'])['Unusual']).count()
+            # n = st.sidebar.slider('Number of features?', 1, counts,3)
             # convert counts to a DataFrame and unstack the 'Status' index level
             counts_df = counts.unstack(level='Unusual', fill_value=0)
 
             st.bar_chart(counts_df)
+            # fig, ax = plt.subplots()
+            # ax.bar(df['Unusual'].head(20), df['Time'].head(20))
+            # st.pyplot(fig)
 
 
         col1,col2 = st.columns(2)
+
+
+
+        # n = st.sidebar.slider('Number of features?', 1, 13,3)
+        # def XGBoost(x, y):
+        #     model = XGBClassifier()
+        #     model.fit(x, y)
+        #     feat_importances = pd.Series(model.feature_importances_, index=x.columns).sort_values(ascending=False)
+        #     top_n_features = feat_importances[:n]
+        #     figure = px.bar(top_n_features,
+        #     x=top_n_features.values,
+        #     y=top_n_features.keys(), labels = {'x':'Importance Value', 'index':'Columns'},
+        #     text=np.round(top_n_features.values, 2),
+        #     title= ' Feature Selection Plot',
+        #             width=1000, height=600)
+        #     figure.update_layout({
+        #             'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+        #             'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+        #     })
+        #     st.plotly_chart(figure)
+        # XGBoost(x,y)
+        #st.pyplot(plot_importance(df).figure)
         # with col1:
         #     st.text("Distribution of Time During Unusual")
         #     fig, ax = plt.subplots()
@@ -135,6 +166,7 @@ if file is not None:
     if opt == 'Data Science Team':
         st.markdown("The source code of this app is available on [Github](https://github.com/Anupriya-Sri/TBC-AIP-2023-A7_Telewire-Analytics)")
     
+
     if opt == 'Summary':
         def get_table(df):
             datatable = df.drop('Unusual',axis=1)
